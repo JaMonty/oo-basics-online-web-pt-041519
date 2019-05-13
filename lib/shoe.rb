5da3f7fa733ced5411142e65a98ab08a6b731315
Jason  Montes <jasone.montes@gmail.com>
2019-05-13 16:30:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
-# Make your shoe class here!
+class Shoe
+ 
+
+def initialize(shoe)
+  @shoe = shoe
+end
+
+def shoe
+  @shoe
+end
+
+
+def initialize(brand)
+  @brand = brand
+end
+
+def brand
+  @brand
+end
+
+def color(color)
+  @color = color
+  
+  def color
+    @color 
+end
+
+
+
+
+
+
+
+end
+end
